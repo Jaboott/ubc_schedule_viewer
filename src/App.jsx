60b6591139d132fd39f6e405dce3ac9df0b033ff
Jason Xu <jasonxuuuu@gmail.com>
2024-08-18 01:58:40 -0700

    Added callback functions to some components to transfer file data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import './App.css';
-import ControlBar from './components/FileUploadScreen'
+import FileUploadSection from './components/FileUpload/FileUploadPage';
+import SchedulePage from './components/Schedule/SchedulePage';
+import readFile from './util/xlsxParser';
 
 function App() {
 
+  const [file, setFile] = useState();
+
+  const handleFileUpload = useCallback(async (uploaded_file) => {
+    readFile(uploaded_file)
+      .then((parsed_file) => {
+        console.log(parsed_file);
+        setFile(parsed_file);
+      })
+  }, [readFile, setFile]);
+
   return (
     <div className="App">
-      <ControlBar></ControlBar>
+      {(file)
+        ? <SchedulePage schedule={file}></SchedulePage>
+        : <FileUploadSection onFileUpload={handleFileUpload}></FileUploadSection>
+      }
     </div>
   );
 }
