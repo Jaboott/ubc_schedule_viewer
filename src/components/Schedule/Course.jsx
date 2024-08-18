c5b12b3b29e9f8a65bfbb570b5683ffe60ad771c
Jason Xu <jasonxuuuu@gmail.com>
2024-08-18 13:00:03 -0700

    Added function to collapse courses with lab or discussion into one object.

--- src/components/Schedule/Course.jsx
+++ src/components/Schedule/Course.jsx
@@ -1,14 +1,14 @@
 function Course({ course }) {
 
     return (
-        <div className="flex flex-col bg-rose-200 pl-2 py-1 mb-3 rounded-md place-content-center">
+        <div className="flex flex-col bg-[#a376aa] pl-2 py-1 mb-4 rounded-md place-content-center">
             <h1 className="text-black">
-                {course ? course["course"].course_code : "Place Holder"}
+                {course ? course.course_code : "Place Holder"}
             </h1>
             <span className="text-xs pb-1 text-black">
-                {course ? course["course"].course_title : "Place Holder"}
+                {course ? course.course_title : "Place Holder"}
             </span>
-        </div >
+        </div>
     );
 }
 
--- src/components/Schedule/CourseList.jsx
+++ src/components/Schedule/CourseList.jsx
@@ -4,11 +4,31 @@ function CourseList({ courses }) {
     // bg-rose-200, bg-yellow-100, bg-violet-300, bg-green-100, bg-blue-200, bg-purple-300
     // Consider these colors
 
+    const CombineCourse = (courses) => {
+        let map = new Map();
+
+        for (const course of courses) {
+            let courseCode = course["course"].course_code;
+
+            if (!map.has(courseCode)) {
+                map.set(courseCode, {
+                    'course_code': courseCode,
+                    'course_title': course["course"].course_title,
+                    'Lecture': null,
+                    'Discussion': null,
+                    'Laboratory': null
+                });
+            }
+            map.get(courseCode)[course["additional"].instructional_format] = course
+        }
+        return Array.from(map.values());
+    }
+
     return (
-        <div className="mx-3 pb-2 flex flex-col">
-            <h1 className="py-2 font-semibold text-lg">Courses</h1>
+        <div className="pt-3 flex flex-col">
+            <h1 className="font-semibold pb-3 text-lg">Courses</h1>
             <div>
-                {courses.map((course, index) => (
+                {CombineCourse(courses).map((course, index) => (
                     <div key={index}>
                         <Course course={course} />
                     </div>
