6487ef1def6a6cc7b7844d612f5cb16e90505144
Jason Xu <jasonxuuuu@gmail.com>
2024-08-18 17:50:16 -0700

    Added expanding and collapsing animation to course component.

--- src/components/Schedule/Course.jsx
+++ src/components/Schedule/Course.jsx
@@ -1,14 +1,51 @@
+import { FiChevronLeft } from "react-icons/fi";
+import { useState, useEffect, useRef } from 'react';
+import CourseDetail from "./CourseDetail";
+
 function Course({ course }) {
 
+    const [expand, setExpand] = useState(false);
+    const contentRef = useRef(null);
+
+    const handleClick = () => {
+        setExpand(!expand);
+    }
+
+    // Resetting the state when Term changes
+    useEffect(() => {
+        setExpand(false);
+    }, [course]);
+
+    // Transition effect for interacting with a course
+    useEffect(() => {
+        if (expand && contentRef.current) {
+            contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+        } else if (contentRef.current || course) {
+            contentRef.current.style.maxHeight = "0px";
+        }
+    }, [expand]);
+
     return (
-        <div className="flex flex-col mb-4 pl-2 py-1 rounded-md place-content-center hover:scale-[1.01] ease-in-out duration-100"
-            style={{ backgroundColor: course.color }}>
-            <h1 className="text-black">
-                {course ? course.course_code : "Place Holder"}
-            </h1>
-            <span className="text-xs pb-1 text-black">
-                {course ? course.course_title : "Place Holder"}
-            </span>
+        <div className="flex flex-col mb-4 rounded-md hover:scale-[1.01]" style={{ backgroundColor: course.color }}>
+            <div className="flex justify-between px-3 py-1 hover:cursor-pointer"
+                onClick={handleClick}>
+                <div className="flex flex-col place-content-center">
+                    <h1 className="text-[#282c34] font-medium">
+                        {course ? course.course_code : "Place Holder"}
+                    </h1>
+                    <span className="text-xs pb-1 text-black">
+                        {course ? course.course_title : "Place Holder"}
+                    </span>
+                </div>
+                <i className={`mr-3 place-content-center ${expand ? "rotate-[-90deg]" : "rotate-0"} ease-in-out duration-100`}><FiChevronLeft className="text-[#696b6f] text-xl" /></i>
+            </div>
+            <div
+                ref={contentRef}
+                className="duration-300 ease-in-out"
+                style={{ maxHeight: "0px" }}
+            >
+                <CourseDetail course={course.course_detail} />
+            </div>
         </div>
     );
 }
---
+++ src/components/Schedule/CourseDetail.jsx
@@ -0,0 +1,14 @@
+function CourseDetail({ course }) {
+
+    if (course) {
+        console.log(course);
+    }
+
+    return (
+        <div className="pb-24">
+            <hr className="border-[#363943] mx-3"></hr>
+        </div>
+    );
+}
+
+export default CourseDetail;
