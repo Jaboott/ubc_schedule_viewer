c79ad399d5b5301486cd61d32b2c24dae247b663
Jason Xu <jasonxuuuu@gmail.com>
2024-08-23 14:26:49 -0700

    Removed the line, changed it so only the current time show.

--- src/components/Schedule/Calendar/TimeLine.jsx
+++ src/components/Schedule/Calendar/TimeLine.jsx
@@ -25,14 +25,14 @@ function TimeLine() {
     return (
         (time > 8 && time < 23) &&
         <div
-            className="absolute z-40 w-full"
+            className="absolute z-40"
             style={{ marginTop: 54 + 56 * (time - 8) }}
         >
             <div className="flex items-center">
                 <div className="flex w-[80px] justify-end">
                     <h1 className="mr-4 text-[#d66d71] bg-[#1e1f28] text-sm font-medium">{convertDecimalTime(time)}</h1>
                 </div>
-                <hr className="border-[#c65f64] grow" />
+                {/* <hr className="border-[#c65f64] grow" /> */}
             </div>
         </div>
     )
