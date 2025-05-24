d007fb972f86ab46a43643f60c82cba0bfc9827a
Jason Xu <jasonxuuuu@gmail.com>
2025-05-24 14:17:01 -0700

    feat: created button to export term content to ics

--- src/components/Schedule/LeftBar.jsx
+++ src/components/Schedule/LeftBar.jsx
@@ -1,6 +1,7 @@
 import CourseList from './Course/CourseList';
 import TermSelector from './TermSelector';
 import ResetFile from './ResetFile';
+import ExportTerm from "./ExportTerm.jsx";
 
 function LeftBar({ courses, term, onTermChange }) {
     if (!courses) return null;
@@ -9,7 +10,10 @@ function LeftBar({ courses, term, onTermChange }) {
         <div className="flex flex-col bg-[#191a21] rounded-lg lg:w-96">
             <div className="flex flex-col mx-4">
                 <TermSelector term={term} onTermChange={onTermChange} />
-                <ResetFile />
+                <div className="flex flex-row justify-between">
+                    <ResetFile />
+                    <ExportTerm courses={courses} />
+                </div>
                 <hr className="border-[#363943]"></hr>
                 <CourseList courses={courses} />
             </div>
