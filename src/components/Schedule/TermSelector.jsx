80e29fdd8301b75be4fe074d3911974b56eb01c7
Jason Xu <jasonxuuuu@gmail.com>
2025-05-24 14:16:10 -0700

    style: minor UI adjustments

--- src/components/Schedule/TermSelector.jsx
+++ src/components/Schedule/TermSelector.jsx
@@ -8,7 +8,7 @@ function TermSelector({ term, onTermChange }) {
     return (
         <div className="pt-5 flex flex-col">
             <h1 className="pb-3 font-semibold text-lg">Schedule For</h1>
-            <select className="text-lg grow py-1 rounded-lg bg-[#272831]" onChange={handleChange} value={term}>
+            <select className="px-2 text-lg grow py-1 rounded-lg bg-[#272831]" onChange={handleChange} value={term}>
                 <option value="term_1">Term 1</option>
                 <option value="term_2">Term 2</option>
                 <option value="summer">Summer</option>
